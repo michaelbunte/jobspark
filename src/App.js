790e55e7224ae620bc56decebd4a066c6eea98b9
Michael Bunte <michaelwbunte@gmail.com>
2024-05-10 02:19:12 -0700

    basic  questionaire format

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Container, Row, Col, BootstrapProvider } from 'react-bootstrap';
+
+function QuestionaireBox({ children }) {
+  return <div style={{
+    width: "100%",
+    display: "flex"
+  }}>
+    <div style={{width: "20px"}}/>
+    <div style={{
+      width: "100%",
+    }}>
+      <div style={{
+        margin: "5px",
+        backgroundColor: "#D9D9D9",
+        borderRadius: "15px",
+        border: "black 2px solid",
+        padding: "3px",
+        maxWidth: "600px",
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}>
+        {children}
+      </div>
+    </div>
+    <div style={{width: "20px"}}/>
+  </div>
+}
+
+function QuestionaireElement({ children }) {
+  return <div style={{
+    borderRadius: "15px",
+    border: "black 2px solid",
+    background: "#80C3E4",
+    boxShadow: "14px 17px 56px -6px rgba(0,0,0,0.81)",
+    margin: "5px",
+    width: "80%"
+  }}>
+    {children}
+  </div>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <QuestionaireBox>
+          <QuestionaireElement>
+            hello
+          </QuestionaireElement>
+        </QuestionaireBox>
       </header>
-    </div>
+    </div >
+
   );
 }
 
