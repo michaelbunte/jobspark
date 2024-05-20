40a5ee6f606d370f34adad93551a614939fa29f6
akashc2172 <162001829+akashc2172@users.noreply.github.com>
2024-05-19 21:48:27 -0700

    Update App.js
    Added link to resources.js, but need to figure out where it links on full website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import EntryQuiz from "./Pages/EntryQuiz";
 import Homepage from "./Pages/Homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import JobSparkResources from "./Pages/Resources";
+
 
 function App() {
   return <BrowserRouter>
     <Routes> 
       <Route path="/">
         <Route path="questionaire" element={<EntryQuiz/>} />
+        <Route path="resources" element={<JobSparkResources />} />
         <Route index element={<Homepage/>}/>
       </Route>
     </Routes>
